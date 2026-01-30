084620b45e7c41912f8ce22a6ee9d7273faefedf
Victoria-Reiss <v.oliveirareiss@gmail.com>
2026-01-30 11:46:29 -0300

    adjust lp

--- src/App.tsx
+++ src/App.tsx
@@ -15,12 +15,14 @@ const App = () => (
       <Toaster />
       <Sonner />
       <Routes>
-        <Route path="/" element={<Index />} />
-        <Route path="*" element={<NotFound />} />
+        <Route path="/" element={<NotFound />} />
+        <Route path="/contabilidade" element={<Index />} />
         <Route path="/formulario" element={<Formulario />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </TooltipProvider>
   </QueryClientProvider>
 );
+
 
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from "react-dom/client";
-// import { BrowserRouter } from "react-router-dom";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+// import { HashRouter } from "react-router-dom";
 import App from "./App.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
-  <HashRouter>
-      <App />
-  </HashRouter>
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
 );
